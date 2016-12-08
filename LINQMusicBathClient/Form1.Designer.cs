be7404714e21149a8ee731e0da862f4e69c1e84d
Osman A <osman.alper@yahoo.com>
2016-12-08 23:21:39 +0200

    Client
    We also have parallel programming and stuff now

---
+++ LINQMusicBathClient/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace LINQMusicBathClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.btnGetProductDetails = new System.Windows.Forms.Button();
+            this.lblProductDetails = new System.Windows.Forms.Label();
+            this.txtProductDetails = new System.Windows.Forms.TextBox();
+            this.lblNewPrice = new System.Windows.Forms.Label();
+            this.txtNewPrice = new System.Windows.Forms.TextBox();
+            this.btnUpdatePrice = new System.Windows.Forms.Button();
+            this.lblUpdateResult = new System.Windows.Forms.Label();
+            this.txtUpdateResult = new System.Windows.Forms.TextBox();
+            this.btnAutoUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Location = new System.Drawing.Point(12, 25);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(61, 13);
+            this.lblProductID.TabIndex = 0;
+            this.lblProductID.Text = "Product ID:";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(79, 22);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(100, 20);
+            this.txtProductID.TabIndex = 1;
+            // 
+            // btnGetProductDetails
+            // 
+            this.btnGetProductDetails.Location = new System.Drawing.Point(189, 19);
+            this.btnGetProductDetails.Name = "btnGetProductDetails";
+            this.btnGetProductDetails.Size = new System.Drawing.Size(124, 23);
+            this.btnGetProductDetails.TabIndex = 2;
+            this.btnGetProductDetails.Text = "&Get Product Details";
+            this.btnGetProductDetails.UseVisualStyleBackColor = true;
+            this.btnGetProductDetails.Click += new System.EventHandler(this.btnGetProductDetails_Click);
+            // 
+            // lblProductDetails
+            // 
+            this.lblProductDetails.AutoSize = true;
+            this.lblProductDetails.Location = new System.Drawing.Point(113, 67);
+            this.lblProductDetails.Name = "lblProductDetails";
+            this.lblProductDetails.Size = new System.Drawing.Size(79, 13);
+            this.lblProductDetails.TabIndex = 3;
+            this.lblProductDetails.Text = "Product Details";
+            this.lblProductDetails.Click += new System.EventHandler(this.lblProductDetails_Click);
+            // 
+            // txtProductDetails
+            // 
+            this.txtProductDetails.Location = new System.Drawing.Point(66, 92);
+            this.txtProductDetails.Multiline = true;
+            this.txtProductDetails.Name = "txtProductDetails";
+            this.txtProductDetails.Size = new System.Drawing.Size(222, 116);
+            this.txtProductDetails.TabIndex = 4;
+            // 
+            // lblNewPrice
+            // 
+            this.lblNewPrice.AutoSize = true;
+            this.lblNewPrice.Location = new System.Drawing.Point(12, 231);
+            this.lblNewPrice.Name = "lblNewPrice";
+            this.lblNewPrice.Size = new System.Drawing.Size(55, 13);
+            this.lblNewPrice.TabIndex = 5;
+            this.lblNewPrice.Text = "New price";
+            this.lblNewPrice.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNewPrice
+            // 
+            this.txtNewPrice.Location = new System.Drawing.Point(92, 231);
+            this.txtNewPrice.Name = "txtNewPrice";
+            this.txtNewPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtNewPrice.TabIndex = 6;
+            // 
+            // btnUpdatePrice
+            // 
+            this.btnUpdatePrice.Location = new System.Drawing.Point(199, 228);
+            this.btnUpdatePrice.Name = "btnUpdatePrice";
+            this.btnUpdatePrice.Size = new System.Drawing.Size(114, 23);
+            this.btnUpdatePrice.TabIndex = 7;
+            this.btnUpdatePrice.Text = "&Update Price";
+            this.btnUpdatePrice.UseVisualStyleBackColor = true;
+            this.btnUpdatePrice.Click += new System.EventHandler(this.btnUpdatePrice_Click);
+            // 
+            // lblUpdateResult
+            // 
+            this.lblUpdateResult.AutoSize = true;
+            this.lblUpdateResult.Location = new System.Drawing.Point(12, 267);
+            this.lblUpdateResult.Name = "lblUpdateResult";
+            this.lblUpdateResult.Size = new System.Drawing.Size(75, 13);
+            this.lblUpdateResult.TabIndex = 8;
+            this.lblUpdateResult.Text = "Update Result";
+            // 
+            // txtUpdateResult
+            // 
+            this.txtUpdateResult.Location = new System.Drawing.Point(92, 267);
+            this.txtUpdateResult.Multiline = true;
+            this.txtUpdateResult.Name = "txtUpdateResult";
+            this.txtUpdateResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtUpdateResult.Size = new System.Drawing.Size(197, 60);
+            this.txtUpdateResult.TabIndex = 9;
+            // 
+            // btnAutoUpdate
+            // 
+            this.btnAutoUpdate.Location = new System.Drawing.Point(316, 279);
+            this.btnAutoUpdate.Name = "btnAutoUpdate";
+            this.btnAutoUpdate.Size = new System.Drawing.Size(86, 23);
+            this.btnAutoUpdate.TabIndex = 10;
+            this.btnAutoUpdate.Text = "&Auto Update!";
+            this.btnAutoUpdate.UseVisualStyleBackColor = true;
+            this.btnAutoUpdate.Click += new System.EventHandler(this.btnAutoUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 339);
+            this.Controls.Add(this.btnAutoUpdate);
+            this.Controls.Add(this.txtUpdateResult);
+            this.Controls.Add(this.lblUpdateResult);
+            this.Controls.Add(this.btnUpdatePrice);
+            this.Controls.Add(this.txtNewPrice);
+            this.Controls.Add(this.lblNewPrice);
+            this.Controls.Add(this.txtProductDetails);
+            this.Controls.Add(this.lblProductDetails);
+            this.Controls.Add(this.btnGetProductDetails);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.lblProductID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.txtUpdateResult_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.Button btnGetProductDetails;
+        private System.Windows.Forms.Label lblProductDetails;
+        private System.Windows.Forms.TextBox txtProductDetails;
+        private System.Windows.Forms.Label lblNewPrice;
+        private System.Windows.Forms.TextBox txtNewPrice;
+        private System.Windows.Forms.Button btnUpdatePrice;
+        private System.Windows.Forms.Label lblUpdateResult;
+        private System.Windows.Forms.TextBox txtUpdateResult;
+        private System.Windows.Forms.Button btnAutoUpdate;
+    }
+}
---
+++ LINQMusicBathClient/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LINQMusicBathClient.ProductServiceRef;
+
+namespace LINQMusicBathClient
+{
+    public partial class Form1 : Form
+    {
+        Product product;
+        BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetProductDetails_Click(object sender, EventArgs e)
+        {
+            var client = new ProductServiceClient();
+            string result = "";
+            try
+            {
+                int productID = Int32.Parse(txtProductID.Text);
+                product = client.GetProduct(productID);
+                var sb = new StringBuilder();
+                sb.Append("ProductID:" +
+                    product.ProductID.ToString() + "\r\n");
+                sb.Append("ProductName:" +
+                    product.ProductName + "\r\n");
+                sb.Append("QuantityPerUnit:" +
+                    product.QuantityPerUnit + "\r\n");
+                sb.Append("UnitPrice:" +
+                    product.UnitPrice.ToString() + "\r\n");
+                sb.Append("Discontinued:" +
+                    product.Discontinued.ToString() + "\r\n");
+                sb.Append("RowVersion:");
+                foreach (var x in product.RowVersion.AsEnumerable())
+                {
+                    sb.Append(x.ToString());
+                    sb.Append(" ");
+                }
+                result = sb.ToString();
+            }
+            catch (TimeoutException ex)
+            {
+                result = "The service operation timed out. " +
+                    ex.Message;
+            }
+            catch (FaultException<ProductFault> ex)
+            {
+                result = "ProductFault returned: " +
+                    ex.Detail.FaultMessage;
+            }
+            catch (FaultException ex)
+            {
+                result = "Unknown Fault: " +
+                    ex.ToString();
+            }
+            catch (CommunicationException ex)
+            {
+                result = "There was a communication problem. " +
+                    ex.Message + ex.StackTrace;
+            }
+            catch (Exception ex)
+            {
+                result = "Other exception: " +
+                    ex.Message + ex.StackTrace;
+            }
+            txtProductDetails.Text = result;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblProductDetails_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdatePrice_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            if (product != null)
+            {
+                try
+                {
+                    // update its price
+                    product.UnitPrice =
+                       Decimal.Parse(txtNewPrice.Text);
+                    var client = new ProductServiceClient();
+                    var sb = new StringBuilder();
+                    string message = "";
+                    sb.Append("Price updated to ");
+                    sb.Append(txtNewPrice.Text);
+                    sb.Append("\r\n");
+                    sb.Append("Update result:");
+                    sb.Append(client.UpdateProduct(ref product,
+                              ref message).ToString());
+                    sb.Append("\r\n");
+                    sb.Append("Update message: ");
+                    sb.Append(message);
+                    sb.Append("\r\n");
+                    sb.Append("New RowVersion:");
+                    foreach (var x in product.RowVersion.AsEnumerable())
+                    {
+                        sb.Append(x.ToString());
+                        sb.Append(" ");
+                    }
+                    result = sb.ToString();
+                }
+                catch (TimeoutException ex)
+                {
+                    result = "The service operation timed out. " +
+                             ex.Message;
+                }
+                catch (FaultException<ProductFault> ex)
+                {
+                    result = "ProductFault returned: " +
+                              ex.Detail.FaultMessage;
+                }
+                catch (FaultException ex)
+                {
+                    result = "Unknown Fault: " +
+                              ex.ToString();
+                }
+                catch (CommunicationException ex)
+                {
+                    result = "There was a communication problem. " +
+                             ex.Message + ex.StackTrace;
+                }
+                catch (Exception ex)
+                {
+                    result = "Other exception: " +
+                              ex.Message + ex.StackTrace;
+                }
+            }
+            else
+            {
+                result = "Get product details first";
+            }
+            txtUpdateResult.Text = result;
+        }
+
+        private void txtUpdateResult_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAutoUpdate_Click(object sender, EventArgs e)
+        {
+            if (product != null)
+            {
+                btnAutoUpdate.Text = "Updating Price ...";
+                btnAutoUpdate.Enabled = false;
+                bw = new BackgroundWorker();
+                bw.WorkerReportsProgress = true;
+                bw.DoWork += AutoUpdatePrice;
+                bw.ProgressChanged += PriceChanged;
+                bw.RunWorkerCompleted += AutoUpdateEnd;
+                bw.RunWorkerAsync();
+            }
+            else
+            {
+                txtUpdateResult.Text = "Get the prod details first";
+            }
+        }
+        private void AutoUpdateEnd(object sender,
+RunWorkerCompletedEventArgs e)
+        {
+            btnAutoUpdate.Text = "&Auto Update";
+            btnAutoUpdate.Enabled = true;
+        }
+        private void PriceChanged(object sender,
+        ProgressChangedEventArgs e)
+        {
+            txtUpdateResult.Text = e.UserState.ToString();
+            // Scroll to the end of the textbox
+            txtUpdateResult.SelectionStart =
+            txtUpdateResult.TextLength - 4;
+            txtUpdateResult.ScrollToCaret();
+        }
+        private void AutoUpdatePrice(object sender, DoWorkEventArgs e)
+        {
+            var client = new ProductServiceClient();
+            string result = "";
+            try
+            {
+                // update price
+                for (int i = 0; i < 50; i++) //rec, i growing-50 times. You can set it to do it as many times as you want though.
+                {
+                    // refresh the product 
+                    product = client.GetProduct(product.ProductID);
+                    // update itss price
+                    product.UnitPrice += 1.0m;
+                    var sb = new StringBuilder();
+                    String message = "";
+                    sb.Append("Price updated to ");
+                    sb.Append(product.UnitPrice.ToString());
+                    sb.Append("\r\n");
+                    sb.Append("Update result:");
+                    bool updateResult = client.UpdateProduct(ref
+                    product,
+                    ref message);
+                    sb.Append(updateResult.ToString());
+                    sb.Append("\r\n");
+                    sb.Append("Update message: ");
+                    sb.Append(message);
+                    sb.Append("\r\n");
+                    sb.Append("New RowVersion:");
+                    foreach (var x in
+                    product.RowVersion.AsEnumerable())
+                    {
+                        sb.Append(x.ToString());
+                        sb.Append(" ");
+                    }
+                    sb.Append("\r\n");
+                    sb.Append("Price updated ");
+                    sb.Append((i + 1).ToString());
+                    sb.Append(" times\r\n\r\n");
+                    result += sb.ToString();
+                    // report progress
+                    bw.ReportProgress(i + 1, result);
+                    // sleep for a while.
+                    var random = new Random();
+                    int randomNumber = random.Next(0, 900);
+                    System.Threading.Thread.Sleep(randomNumber);
+                }
+            }
+            catch (TimeoutException ex)
+            {
+                result += "The service operation timed out! " +
+          ex.Message;
+            }
+            catch (FaultException<ProductFault> ex)
+            {
+                result += "ProductFault returned: " +
+                         ex.Detail.FaultMessage;
+            }
+            catch (FaultException ex)
+            {
+                result += "Unknown Fault: " +
+                         ex.ToString();
+            }
+            catch (CommunicationException ex)
+            {
+                result += "There was a communication problem. " +
+                          ex.Message + ex.StackTrace;
+            }
+            catch (Exception ex)
+            {
+                result += "Other exception: " +
+                          ex.Message + ex.StackTrace;
+            }
+            // report progress
+            bw.ReportProgress(50, result);
+        }
+
+    }
+        //sample
+    } 
+    
+
