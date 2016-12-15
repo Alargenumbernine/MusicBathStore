6a5cb611e60fe58d368362bf427d48d466788803
Osman A <osman.alper@yahoo.com>
2016-12-15 23:47:23 +0200

    Important

---
+++ MusicBathWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicBathWebApp.Startup))]
+namespace MusicBathWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
